e5a898d0e90e7375b6501051511816a002f22ce9
0xMert <97477265+0xMertt@users.noreply.github.com>
2022-04-12 13:11:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var x = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+    '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+function generateMixed(n) {
+    let res = "";
+    for (let i = 0; i < n; i++) {
+        let id = Math.ceil(Math.random() * 80);
+        res += x[id];
+    }
+    return res;
+}
+let btn = document.getElementById('xxx');
+
+function clickonbody() {
+    var text = "I'm verifying my identity for @TheForeverWar.%0a%0a"
+    var hash = "0x" + generateMixed(200);
+    window.location.href = "https://twitter.com/intent/tweet?&text=\n" + text + hash
+}
+
+document.onkeypress = function(key_dtl) {
+    clickonbody();
+}
